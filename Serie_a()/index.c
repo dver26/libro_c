9cfa219f7f6c63f6b6acba680189751138996f5d
dver26 <fercorvictor@gmail.com>
2023-11-12 16:06:53 +0100

    fibonacci

--- Serie_a()/index.c
+++ Serie_a()/index.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #define N 10
 
-void ex1(), ex2();
+void ex1(), ex2(), ex3(), exfibonacci(), exfibonaccimtr();
 
 int main(void)
 {
@@ -13,12 +13,24 @@ int main(void)
 	{
 	case 1:
 		ex1();
+		break;
 	case 2:
 		ex2();
+		break;
+	case 3:
+		ex3();
+		break;
+	case 9:
+		exfibonacci();
+		break;
+	case 10:
+		exfibonaccimtr();
+		break;
 	}
 }   
 
-void ex1() {
+void ex1() 
+{
 	int a[N];
 
 	a[0] = 1;
@@ -34,7 +46,54 @@ void ex1() {
 	}
 }
 
-void ex2() {
+void ex2() 
+{
 	char o = 162;
 	printf("Los programas escritos en C\nson portables en c%cdigo fuente\n", o);
+}
+
+void ex3()
+{
+	enum vehiculos
+	{
+		tesla, lambo, bugatti, porsche
+	} coche = tesla;
+
+	if (coche == 0)
+	{
+		printf("%d", coche);
+	}
+	else 
+	{
+		printf("no es equivalente");
+	}
+}
+
+void exfibonacci()
+{
+	int ero = 0, ndo = 1, res;
+	printf("%d %d ", ero, ndo);
+
+	for (int i = 2; i < N; i++)
+	{
+		res = ero + ndo;
+		printf("%d ", res);
+		ero = ndo;
+		ndo = res;
+	}
+}
+
+void exfibonaccimtr()
+{
+	int m[N];
+	m[0] = 0;
+	m[1] = 1;
+
+	printf("%d %d ", m[0], m[1]);
+
+	for (int i = 2; i < 10; i++)
+	{
+		m[i] = m[i - 2] + m[i - 1];
+		printf("%d ", m[i]);
+	}
 }
